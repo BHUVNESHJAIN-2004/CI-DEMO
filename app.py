7952815793faf2a1208dc984882fd0807da1c96b
Bhuvnesh Jain <bhuvneshjain2004@gmail.com>
2026-09-24 09:02:58 +0530

    fix code issues

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b):
+    return a - b
 
 
+res1 = add(4, 5)
+res2 = sub(4, 1)
 
-
-def sub(a,b):return a-b
-
-res1 = add(4,5)
-res2 = sub(4,1)
-
-print(res1,res2)
+print(res1, res2)
